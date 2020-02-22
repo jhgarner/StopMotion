ab1b2fd8659057e592831b666c6e1acff5094f62
Ryker Fish <rfish@mymail.mines.edu>
2020-02-22 15:42:34 -0700

    Added buttons to activity_main that swap to activity_video_viewer (the editor) and to activity_ux (the camera)

--- app/src/main/java/com/example/stopmotion/MainActivity.kt
+++ app/src/main/java/com/example/stopmotion/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.stopmotion
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
@@ -13,11 +15,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "I hope this works", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+        }
+
+        editorBtn.setOnClickListener {
+            val intent = Intent(this, VideoViewer::class.java)
+            startActivity(intent)
+        }
+
+        cameraBtn.setOnClickListener {
+            val intent = Intent(this, HelloSceneformActivity::class.java)
+            startActivity(intent)
         }
     }
 
@@ -36,4 +47,9 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+
+
+
+
 }
